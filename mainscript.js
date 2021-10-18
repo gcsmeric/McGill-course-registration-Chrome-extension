7fa0c356bc7931b33703e912017711b8e0cd001c
Geoffrey Meric <gcs.meric@gmail.com>
2021-10-18 12:32:58 -0400

    updated

--- mainscript.js
+++ mainscript.js
@@ -5,6 +5,35 @@
 
 //fetching instructor names section from mcgill course webpage
 //COMMENT MORE
+
+//https://stackoverflow.com/questions/60471911/http-request-to-a-website-to-get-the-content-of-a-specific-html-element
+
+
+//WATCH THIS https://www.youtube.com/watch?v=yC7RvrsgLCI
+
+async function FetchHtml(url) {
+    let response = await fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            alert("could not fetch");
+            throw new Error("Could not reach website.");
+        }
+        return response.json();
+    })
+    return await response.text(); // Returns it as Promise
+}
+async function getHTML(url) {
+    let html = await FetchHtml(url).then(text => {return text}); // Get html from the promise
+    alert(html);
+}
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+const profIDRegex = /ShowRatings\.jsp\?tid=[0-9]*/g;
+
 instructors = document.getElementsByClassName("catalog-instructors")[0].innerHTML;
 //split string into array according to following delimiters: , ; : ( )
 instArray = instructors.split(/,|;|:|\(|\)/);
@@ -40,10 +69,25 @@ if (instArray[0]=="Instructors") {
     //iterate for each prof
     for (j=0; j<nameIndices.length; j++) {
         //create query url for ratemyprofessors.com query finding prof (search by prof name at mcgill university)
-        var url = 'https://www.ratemyprofessors.com/search/teachers?query='
+        var urlvar = 'https://www.ratemyprofessors.com/search/teachers?query='
         +instArray[nameIndices[j]]+'+'+instArray[nameIndices[j]+1]
         +'&sid=U2Nob29sLTE0Mzk=';
-        alert(url);
+        alert(urlvar);
+
+        getHTML('https://google.com');
+          
+        /*$.get(urlvar, function(responseText) {
+            alert(responseText);
+        });*/
+
+        /*var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                alert(xhr.responseText);
+            }
+        }
+        xhr.open('GET', 'http://google.com', true);
+        xhr.send(null);*/
     }
 
 }
